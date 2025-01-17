adb8fb5d6e692375954e2ef5d2c3f4292ee8d480
Bastian Allgeier <mail@bastianallgeier.com>
2025-01-17 13:46:47 +0100

    Add unit tests

--- tests/Discord/DiscordTest.php
+++ tests/Discord/DiscordTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Kirby\Discord\Author;
 use Kirby\Discord\Discord;
 
 describe('Discord', function () {
@@ -29,6 +30,54 @@ describe('Discord', function () {
 		]);
 	});
 
+	describe('should have an author', function () {
+		it('should be creatable from array', function () {
+			$response = Discord::submit(
+				webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+				author: $author = [
+					'icon' => 'https://getkirby.com/icon.png',
+					'name' => 'Test author',
+					'url'  => 'https://getkirby.com'
+				],
+				dryrun: true
+			);
+
+			expect($response['embeds'][0]['author'])->toBe([
+				'icon_url' => $author['icon'],
+				'name'     => $author['name'],
+				'url'      => $author['url']
+			]);
+		});
+
+		it('should be creatable from object', function () {
+			$response = Discord::submit(
+				webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+				author: $author = new Author(
+					icon: 'https://getkirby.com/icon.png',
+					name: 'Test author',
+					url: 'https://getkirby.com'
+				),
+				dryrun: true
+			);
+
+			expect($response['embeds'][0]['author'])->toBe([
+				'icon_url' => $author->icon,
+				'name'     => $author->name,
+				'url'      => $author->url
+			]);
+		});
+	});
+
+	it('should have an avatar', function () {
+		$response = Discord::submit(
+			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+			avatar: $avatar = 'https://example.com/avatar.jpg',
+			dryrun: true
+		);
+
+		expect($response['avatar_url'])->toBe($avatar);
+	});
+
 	it('should have content', function () {
 		$response = Discord::submit(
 			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
@@ -37,6 +86,16 @@ describe('Discord', function () {
 		);
 
 		expect($response['content'])->toBe($content);
+	});
+
+	it('should have a description', function () {
+		$response = Discord::submit(
+			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+			description: $description = 'Test Description',
+			dryrun: true
+		);
+
+		expect($response['embeds'][0]['description'])->toBe($description);
 	});
 
 	it('should have fields', function () {
@@ -71,6 +130,26 @@ describe('Discord', function () {
 
 	});
 
+	it('should have a footer', function () {
+		$response = Discord::submit(
+			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+			footer: $footer = 'Test footer',
+			dryrun: true
+		);
+
+		expect($response['embeds'][0]['footer']['text'])->toBe($footer);
+	});
+
+	it('should have an image', function () {
+		$response = Discord::submit(
+			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+			image: $image = 'https://example.com/image.jpg',
+			dryrun: true
+		);
+
+		expect($response['embeds'][0]['image']['url'])->toBe($image);
+	});
+
 	it('should have a thumbnail', function () {
 		$response = Discord::submit(
 			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
@@ -82,8 +161,7 @@ describe('Discord', function () {
 	});
 
 	describe('should have a timestamp', function () {
-
-		it('from string', function () {
+		it('should be creatable from string', function () {
 			$response = Discord::submit(
 				webhook: 'https://discord.com/api/webhooks/xxx/xxx',
 				timestamp: $ts = '2025-01-17T09:55:24.000Z',
@@ -93,7 +171,7 @@ describe('Discord', function () {
 			expect($response['embeds'][0]['timestamp'])->toBe($ts);
 		});
 
-		it('from DateTime object', function () {
+		it('should be creatable from DateTime object', function () {
 			$response = Discord::submit(
 				webhook: 'https://discord.com/api/webhooks/xxx/xxx',
 				timestamp: new DateTime('2025-01-01'),
@@ -102,7 +180,25 @@ describe('Discord', function () {
 
 			expect($response['embeds'][0]['timestamp'])->toBe('2025-01-01T00:00:00.000Z');
 		});
-
 	});
 
+	it('should have a title', function () {
+		$response = Discord::submit(
+			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+			title: $title = 'Test',
+			dryrun: true
+		);
+
+		expect($response['embeds'][0]['title'])->toBe($title);
+	});
+
+	it('should have a username', function () {
+		$response = Discord::submit(
+			webhook: 'https://discord.com/api/webhooks/xxx/xxx',
+			username: $username = 'test user',
+			dryrun: true
+		);
+
+		expect($response['username'])->toBe($username);
+	});
 });
