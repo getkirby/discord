938e96ec84ee98f24f267c5171c016103c9b8ba0
Bastian Allgeier <mail@bastianallgeier.com>
2025-01-21 21:30:07 +0100

    Fix CS issue

--- src/Author.php
+++ src/Author.php
@@ -23,7 +23,7 @@ class Author
 		string|null $default = null
 	): string {
 		$address = strtolower(trim($email));
-		$hash    = hash('sha256',$address);
+		$hash    = hash('sha256', $address);
 
 		return 'https://www.gravatar.com/avatar/' . $hash . '?d=' . $default;
 	}
